4c416a4c1329dbae39d36b13a535153ae9c48c6b
matija8 <matijanme@gmail.com>
2020-02-29 22:56:02 +0100

    Created the sudoku table view.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,9 @@
+const [rows, cols] = [9, 9];
+
 const main = () => {
     console.log("Scripts loaded!");
-}
+    const root = document.getElementById('root');
+    makeSudoku({root, rows, cols});
+};
 
 main();
--- javascripts/view.js
+++ javascripts/view.js
@@ -1,3 +1,21 @@
-const makeUi = root => {
-    //TODO
-}
+const makeSudoku = ({root, rows, cols}) => {
+    const tableSudoku = document.createElement('div');
+    tableSudoku.classList.add('sudoku-table');
+    const matrix = [];
+
+    for (let i = 0; i < rows; i++) {
+        matrix[i] = [];
+        const rowSudoku = document.createElement('div');
+        rowSudoku.classList.add('sudoku-row');
+        tableSudoku.appendChild(rowSudoku);
+
+        for (let j = 0; j < cols; j++) {
+            const cellSudoku = document.createElement('div');
+            cellSudoku.classList.add('sudoku-cell');
+            cellSudoku.innerText = `${i},${j}`;
+            matrix[i][j] = cellSudoku;
+            rowSudoku.appendChild(cellSudoku);
+        }
+    }
+    root.appendChild(tableSudoku);
+};
