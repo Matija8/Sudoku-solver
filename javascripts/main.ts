d27e3b53f2a0d9e27deaf8d08b756c4de1eaa4f7
matija8 <matijanme@gmail.com>
2020-03-02 00:56:59 +0100

    Added writing numbers into cells.

--- javascripts/main.ts
+++ javascripts/main.ts
@@ -1,33 +1,77 @@
 
-type SudokuValue = null|1|2|3|4|5|6|7|8|9;
+type SudokuValue = null | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
 
-interface SudokuCell {
-    row: number;
-    col: number;
-    val: SudokuValue;
+interface ActiveCell {
+    row: number | null;
+    col: number | null;
 }
 
-const makeSudoku = (root: HTMLElement): WeakMap<HTMLElement, SudokuCell> => {
+
+const presets = Object.freeze({
+    unFocusedColor: 'white',
+    focusedColor: 'grey',
+});
+
+
+const addClickListener = (
+    row: number,
+    col: number,
+    cell: HTMLElement,
+    //matrix: SudokuValue[][],
+    activeCell: ActiveCell
+) => {
+    cell.addEventListener('focus', () => {
+        activeCell.row = row;
+        activeCell.col = col;
+    });
+    cell.addEventListener('blur', () => {
+        activeCell.row = null;
+        activeCell.col = null;
+    });
+}
+
+
+const addKeyPressListener = (activeCell: ActiveCell, DOMmatrix: HTMLElement[][]) => {
+    document.addEventListener('keypress', event => {
+        const n = parseInt(event.key);
+        if (n > 0 && activeCell.row !== null && activeCell.col !== null) {
+            const cell = DOMmatrix[activeCell.row][activeCell.col];
+            cell.innerText = String(n);
+        }
+    });
+}
+
+
+const makeSudoku = (root: HTMLElement): SudokuValue[][] => {
     const sudokuTable = document.createElement('div');
     sudokuTable.classList.add('sudoku-table');
-    const map: WeakMap<HTMLElement, SudokuCell> = new WeakMap();
+    const matrix: SudokuValue[][] = [];
+    const DOMmatrix: HTMLElement[][] = [];
+    const activeCell: ActiveCell = { row: null, col: null };
 
     for (let i = 0; i < 9; i++) {
         const sudokuRow = document.createElement('div');
         sudokuRow.classList.add('sudoku-row');
         sudokuTable.appendChild(sudokuRow);
 
+        matrix[i] = [];
+        DOMmatrix[i] = [];
+
         for (let j = 0; j < 9; j++) {
             const sudokuCell = document.createElement('div');
             sudokuCell.classList.add('sudoku-cell');
             sudokuCell.setAttribute('tabindex', '-1');
-            map.set(sudokuCell, {row: i, col: j, val: null});
             sudokuRow.appendChild(sudokuCell);
+            DOMmatrix[i][j] = sudokuCell;
+            matrix[i][j] = null;
+            addClickListener(i, j, sudokuCell, /*matrix,*/ activeCell);
         }
     }
+    addKeyPressListener(activeCell, DOMmatrix);
     root.appendChild(sudokuTable);
-    return map;
+    return matrix;
 };
+
 
 const main = () => {
     const root = document.getElementById('root');
@@ -35,7 +79,7 @@ const main = () => {
         console.log(`Couldn't get the root element for the sudoku board.`);
         return;
     }
-    const map: WeakMap<HTMLElement, SudokuCell> = makeSudoku(root);
+    const matrix: SudokuValue[][] = makeSudoku(root);
 }
 
 main();
