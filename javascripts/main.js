2296e3c7365cd9230e9f14b6f5a619fc79a3b830
matija8 <matijanme@gmail.com>
2020-03-01 20:52:20 +0100

    Added writing into cells. Made a mess, TODO: refactor.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,60 +1,101 @@
-const [rows, cols] = [9, 9];
+'use-strict';
 
-const makeSudoku = ({root, rows, cols}) => {
+const makeSudoku = ({root}) => {
     const tableSudoku = document.createElement('div');
     tableSudoku.classList.add('sudoku-table');
     const matrix = [];
 
-    for (let i = 0; i < rows; i++) {
+    for (let i = 0; i < 9; i++) {
         matrix[i] = [];
         const rowSudoku = document.createElement('div');
         rowSudoku.classList.add('sudoku-row');
         tableSudoku.appendChild(rowSudoku);
 
-        for (let j = 0; j < cols; j++) {
+        for (let j = 0; j < 9; j++) {
             const cellSudoku = document.createElement('div');
             cellSudoku.classList.add('sudoku-cell');
-            cellSudoku.innerText = `${i},${j}`;
+            //cellSudoku.innerText = `${i},${j}`;
             cellSudoku.setAttribute('tabindex', '-1');
+            cellSudoku.setAttribute('data-row', i);
+            cellSudoku.setAttribute('data-col', j);
             matrix[i][j] = {
                 row: i,
                 col: j,
-                val: null,
                 dom: cellSudoku,
+                val: null,
             };
             rowSudoku.appendChild(cellSudoku);
         }
     }
     root.appendChild(tableSudoku);
-    return matrix;
+    return {
+        table: tableSudoku,
+        matrix: matrix
+    };
 };
 
 
-addClickListeners = matrix => {
-    matrix.forEach(row => {
-        row.forEach(cell => {
-            const {row: i, col: j, dom} = cell;
+const addListeners = state => {
 
-            dom.addEventListener('focus', event => {
-                console.log(`focus ${i},${j}`);
-                //TODO: allow setting sudoku cell value
+    const addClickListeners = state => {
+        state.matrix.forEach(row => {
+            row.forEach(cell => {
+                const {row: i, col: j, dom: cellDOM} = cell;
+
+                cellDOM.addEventListener('focus', event => {
+                    //console.log(`focus ${i},${j}`);
+                    state.focused = cellDOM;
+                    cellDOM.style.backgroundColor = state.focusedColor;
+                });
+
+                cellDOM.addEventListener('blur', event => {
+                    //console.log(`blur  ${i},${j}`);
+                    state.focused = null;
+                    cellDOM.style.backgroundColor = state.unFocusedColor;
+                });
+
             });
-
-            dom.addEventListener('blur', event => {
-                console.log(`blur  ${i},${j}`);
-                //TODO: return prev value;
-            });
-
         });
-    });
+    };
+
+    const addKeyPressListeners = state => {
+        document.addEventListener('keypress', event => {
+            if (event.keyCode === 13) {
+                //console.table(state.matrix);
+            }
+
+            const n = parseInt(event.key);
+            if (n > 0) {
+                //console.log(`Keypress ${n}`);
+                const activeCell = state.focused;
+                if (activeCell) {
+                    activeCell.innerText = n;
+                    const row = activeCell.getAttribute('data-row'),
+                          col = activeCell.getAttribute('data-col');
+                    state.matrix[row][col].val = n;
+                }
+            }
+        });
+    };
+
+    addClickListeners(state);
+    addKeyPressListeners(state);
 };
+
+
+
 
 
 const main = () => {
     console.log("Scripts loaded!");
     const root = document.getElementById('root');
-    const matrix = makeSudoku({root, rows, cols});
-    addClickListeners(matrix);
+    const state = {
+        focused: null,
+        unFocusedColor: 'white',
+        focusedColor: 'grey',
+        matrix: makeSudoku({root}).matrix,
+    };
+    addListeners(state);
 };
 
 
